7f7d2faf9b36367ba305618b66f3ce305f9839d6
sapatamuku-creator <sapatamuku@gmail.com>
2026-05-14 14:08:32 +0700

    Sync: 14/05/2026 14:08:32,86

--- add_v.ps1
+++ add_v.ps1
@@ -4,7 +4,7 @@ foreach ($file in $files) {
     $content = Get-Content $file.FullName -Raw
     if ($content -like "*subdomain_resolver.js*") {
         # Ganti subdomain_resolver.js atau subdomain_resolver.js?v=... dengan versi terbaru
-        $newContent = $content -replace 'subdomain_resolver\.js(\?v=[0-9\.]+)?', 'subdomain_resolver.js?v=3.0'
+        $newContent = $content -replace 'subdomain_resolver\.js(\?v=[0-9\.]+)?', 'subdomain_resolver.js?v=3.1'
         if ($content -ne $newContent) {
             Write-Host "Adding cache-buster to: $($file.Name)"
             Set-Content $file.FullName $newContent -NoNewline
