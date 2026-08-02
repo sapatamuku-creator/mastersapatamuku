b78e319cf8a5094f442b7f354d18070703e8a177
sapatamuku-creator <sapatamuku@gmail.com>
2026-08-03 00:03:20 +0700

    revert: 100% match with stable v2.7 releases code

--- api/sortir-list-drive.js
+++ api/sortir-list-drive.js
@@ -10,7 +10,7 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 // Helper: fetch one page of Drive files with given query
 async function driveList(q, pageToken = null) {
-  const fields = 'nextPageToken,files(id,name,mimeType,parents,thumbnailLink)';
+  const fields = 'nextPageToken,files(id,name,mimeType,parents)';
   let url = `https://www.googleapis.com/drive/v3/files?q=${encodeURIComponent(q)}&key=${GOOGLE_API_KEY}&fields=${encodeURIComponent(fields)}&pageSize=1000&orderBy=name`;
   if (pageToken) url += `&pageToken=${pageToken}`;
   const res = await fetch(url);
